17adbd9647342ef657460cc9a8a36462fa2100c1
flo <flo@MacBook-Pro-de-Camelia.local>
2024-05-16 14:01:57 +0200

    import event default

--- dbms/init-mongo.sh
+++ dbms/init-mongo.sh
@@ -3,4 +3,4 @@
 #mongoimport --host mongo:27017 --username root --password foobar --db yummy-yams --collection pastries --authenticationDatabase=admin --file /usr/src/pastries.json --jsonArray
 
 mongoimport --host ${MONGO_HOST:-mongo} --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --db yummy-yams --collection pastries --authenticationDatabase=admin --file /usr/src/app/pastries.json --jsonArray
-mongoimport --host ${MONGO_HOST:-mongo} --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --db yummy-yams --collection event --authenticationDatabase=admin --file /usr/src/app/event.json --jsonArray
+mongoimport --host ${MONGO_HOST:-mongo} --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --db yummy-yams --collection events --authenticationDatabase=admin --file /usr/src/app/events.json --jsonArray
